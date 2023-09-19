1558abae060fa77973e7730205cea8c7a6f9ab3d
VerduciLuca <135212650+VerduciLuca@users.noreply.github.com>
2023-09-19 13:15:07 +0200

    zxcvzxv

--- src/app/components/favourites/favourites.component.ts
+++ src/app/components/favourites/favourites.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Post } from 'src/app/model/post';
 import { StorageService } from 'src/app/services/storage.service';
 
@@ -8,12 +8,18 @@ import { StorageService } from 'src/app/services/storage.service';
   templateUrl: './favourites.component.html',
   styleUrls: ['./favourites.component.scss']
 })
-export class FavouritesComponent {
+export class FavouritesComponent implements OnInit{
   favs: Post[] = []
   constructor(public storage:StorageService){
-    if (localStorage.getItem('favourites')) {
-      this.favs = JSON.parse(localStorage.getItem('favourites')!)
-    }
+    // if (localStorage.getItem('favourites')) {
+    //   this.favs = JSON.parse(localStorage.getItem('favourites')!)
+    // }
+  }
+  ngOnInit(): void {
+    this.storage.favouritesSubject.subscribe(arrayOfFavourites => {
+    this.favs = arrayOfFavourites
+    this.favs.sort((f1,f2) => f2.created - f1.created)
+  })
   }
 
 
